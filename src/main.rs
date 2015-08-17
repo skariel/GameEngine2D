c148d7e1aa0d658746ba27a42b883aab16e53429
Ariel Keselman <skariel@gmail.com>
2015-08-17 15:53:34 +0300

    implemented aspect ratio

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,9 @@ fn main() {
 
     let t1 = get_triangle(&mg);
 
+    let mut tx = 0.0;
+    let mut ty = 0.0;
+
     loop {
 
         if mg.mouse.left != graphics::mouse::ButtonState::Pressed {
@@ -23,12 +26,17 @@ fn main() {
 
         let zoom = 1500.0/(mg.window.size_pixels_x as f32);
         for i in 1..50 {
-            mg.print(&t1, 0.0, 0.0, 3.0*t*(i as f32)/100.0,zoom,zoom);
+            mg.print(&t1, tx, ty, 3.0*t*(i as f32)/100.0,zoom,zoom);
         }
         mg.flush();
 
         println!("framerate: {}", mg.framerate.get_framerate());
         println!("mouse x,y: {},{} left {:?}, right {:?}", mg.mouse.x, mg.mouse.y,mg.mouse.left,mg.mouse.right);
+
+        if mg.mouse.dleft == graphics::mouse::DButtonState::Pressed {
+            tx = mg.mouse.x;
+            ty = mg.mouse.y;
+        }
 
         if mg.closed {
             return
